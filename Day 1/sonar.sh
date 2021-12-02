83676223eec7f37c5420541cb57c53e1c1e83ffe
MrBananaPants <joran.vancoillie@gmail.com>
2021-12-02 22:18:28 +0100

    Translated to English

--- Day 1/sonar.sh
+++ Day 1/sonar.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 DATA=$1
-LENGTE=$(cat $DATA | wc -l )
+LENGTH=$(cat $DATA | wc -l )
 START=1
 COUNTER=0
-TAIL=$LENGTE
-MINDER=$((LENGTE-1))
+TAIL=$LENGTH
+LENGTH_LESS=$((LENGTH-1))
 
-until [ $START -gt $MINDER ]
+until [ $START -gt $LENGTH_LESS ]
 do
-    HUIDIG=$(tail -n $TAIL $DATA | head -n 2)
-    HUIDIGEWAARDE=$(echo $HUIDIG | sed "s/^[0-9]*//g")
-    VORIGEWAARDE=$(echo $HUIDIG | sed "s/[0-9]*$//g")
-    if [ $HUIDIGEWAARDE -gt $VORIGEWAARDE ]; 
+    CURRENT_PAIR=$(tail -n $TAIL $DATA | head -n 2)
+    CURRENT_VALUE=$(echo $CURRENT_PAIR | sed "s/^[0-9]*//g")
+    PREVIOUS_VALUE=$(echo $CURRENT_PAIR | sed "s/[0-9]*$//g")
+    if [ $CURRENT_VALUE -gt $PREVIOUS_VALUE ]; 
         then
             ((COUNTER=COUNTER+1))
         fi
 
-    TAIL=$((LENGTE-START))
+    TAIL=$((LENGTH_LESS-START))
     ((START=START+1))
 done
 
