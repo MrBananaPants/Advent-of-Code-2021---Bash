37e4a6ea1773c7aceec4d40df797d197bc4713de
jorvcoil <jorvcoil@helios.UGent.be>
2021-12-02 11:57:41 +0100

    My solution for Day 2

---
+++ Day 2/dive.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+DATA=$1
+FORWARD=$(cat $DATA | egrep "^forward" | sed "s/^[a-z]* //g" | sed -z "s/\n/+/g" | sed "s/+$//g")
+FORWARD_TOTAL=$(echo $FORWARD | bc )
+UP=$(cat $DATA | egrep "^up" | sed "s/^[a-z]* //g" | sed -z "s/\n/+/g" | sed "s/+$//g")
+UP_TOTAL=$(echo $UP | bc )
+DOWN=$(cat $DATA | egrep "^down" | sed "s/^[a-z]* //g" | sed -z "s/\n/+/g" | sed "s/+$//g")
+DOWN_TOTAL=$(echo $DOWN | bc )
+
+#echo "$FORWARD -> $FORWARD_TOTAL"
+#echo "$UP -> $UP_TOTAL"
+#echo "$DOWN -> $DOWN_TOTAL"
+
+if [$UP_TOTAL -gt $DOWN_TOTAL];
+    then
+    VERTICAL=$((UP_TOTAL-DOWN_TOTAL))
+    else
+    VERTICAL=$((DOWN_TOTAL-UP_TOTAL))
+    fi
+
+ANSWER=$((FORWARD_TOTAL*VERTICAL))
+
+echo "$ANSWER"
