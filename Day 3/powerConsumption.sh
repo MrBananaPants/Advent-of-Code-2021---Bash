3fe98e5a0431f8b267f95de2d87e104b84a6e3e4
MrBananaPants <joran.vancoillie@gmail.com>
2021-12-03 09:31:46 +0100

    Added solution for day 3

---
+++ Day 3/powerConsumption.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+LENGTH=$(cat $1 | wc -l)
+WIDTH=$(cat $1 | head -n 1 | wc -m)
+((HALF_LENGTH=LENGTH/2))
+
+START=1
+
+while [ $START -lt $WIDTH ] 
+    do
+    SUM=$(cut -b $START $1 | tr '\n' '+' | sed "s/+$//g")
+    SUM=$(echo $SUM | bc)
+    if [ $SUM -gt $HALF_LENGTH ]
+        then
+        RESULT+=1
+        else
+        RESULT+=0
+    fi
+    ((START++))
+done
+
+RESULT_INVERSE=$(tr '01' '10' <<<"$RESULT")
+RESULT_DECIMAL=$(echo "$((2#$RESULT))")
+RESULT_INVERSE_DECIMAL=$(echo "$((2#$RESULT_INVERSE))")
+
+((FINAL_RESULT=RESULT_DECIMAL*RESULT_INVERSE_DECIMAL))
+echo "$FINAL_RESULT"
