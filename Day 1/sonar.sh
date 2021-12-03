37c05306c06cd47d1a848ab42fa7f5f4466b9537
MrBananaPants <joran.vancoillie@gmail.com>
2021-12-03 11:01:58 +0100

    Improved formatting

--- Day 1/sonar.sh
+++ Day 1/sonar.sh
@@ -1,24 +1,22 @@
 #! /bin/bash
 
 DATA=$1
-LENGTH=$(cat $DATA | wc -l )
+LENGTH=$(cat $DATA | wc -l)
 START=1
 COUNTER=0
 TAIL=$LENGTH
-LENGTH_LESS=$((LENGTH-1))
+LENGTH_LESS=$((LENGTH - 1))
 
-until [ $START -gt $LENGTH_LESS ]
-do
+until [ $START -gt $LENGTH_LESS ]; do
     CURRENT_PAIR=$(tail -n $TAIL $DATA | head -n 2)
     CURRENT_VALUE=$(echo $CURRENT_PAIR | sed "s/^[0-9]*//g")
     PREVIOUS_VALUE=$(echo $CURRENT_PAIR | sed "s/[0-9]*$//g")
-    if [ $CURRENT_VALUE -gt $PREVIOUS_VALUE ]; 
-        then
-            ((COUNTER=COUNTER+1))
-        fi
+    if [ $CURRENT_VALUE -gt $PREVIOUS_VALUE ]; then
+        ((COUNTER = COUNTER + 1))
+    fi
 
-    TAIL=$((LENGTH_LESS-START))
-    ((START=START+1))
+    TAIL=$((LENGTH_LESS - START))
+    ((START = START + 1))
 done
 
 echo $COUNTER
